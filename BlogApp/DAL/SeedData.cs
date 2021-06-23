65673ce7b634238c7b00496d43fc0837ef5898fe
Aamon <33285359+44m0n@users.noreply.github.com>
2021-06-23 13:20:37 +0300

    Update SeedData.cs

--- BlogApp/DAL/SeedData.cs
+++ BlogApp/DAL/SeedData.cs
@@ -126,7 +126,7 @@ namespace BlogApp.Dotnet.DAL
                     Content = "Only testuser2 and admin can modify this post. You need to login to comment to this post. \n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Cras efficitur turpis in porttitor commodo. Etiam facilisis, eros euismod sagittis lacinia, justo libero tempor ligula, interdum sodales dui eros ut ipsum. Cras ac vestibulum tellus. Fusce sed libero ligula. Duis dapibus lorem vel mauris commodo suscipit. Curabitur in venenatis dolor, sit amet vestibulum sapien. Nam mauris ligula, laoreet in lobortis ac, faucibus ut mi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer dignissim tortor a libero accumsan venenatis. Nullam ullamcorper urna in purus feugiat, vitae blandit diam consequat. Quisque fermentum eu nisl at convallis. Proin feugiat tortor nibh, accumsan scelerisque ipsum viverra eu. Proin id tortor eleifend lorem porta consequat ut eget nisl. Sed vitae commodo lectus. Aliquam ac eros eget libero venenatis mattis in eu risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
                     CreatedAt = DateTime.Parse("1989-2-12"),
                     ModifiedAt = DateTime.Parse("1989-2-12"),
-                    UserID = "testuser"
+                    UserID = "testuser2"
                 },
 
                 new BlogPost
