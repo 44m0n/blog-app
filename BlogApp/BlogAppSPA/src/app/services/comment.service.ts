d8f8f39776f48436aa2fe5b8ce07d2260501ecd3
Aamon <33285359+44m0n@users.noreply.github.com>
2021-06-28 08:53:26 +0300

    Refactor Services
    Comments services returns now a Promise instead of the result of the Promise.

--- BlogApp/BlogAppSPA/src/app/services/comment.service.ts
+++ BlogApp/BlogAppSPA/src/app/services/comment.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { Page } from '../models/page.model';
 import { Comment } from '../models/comment.model';
 
@@ -16,35 +16,35 @@ export class CommentService {
 
   constructor(private client: HttpClient) { }
 
-  public getAll(postID: string, page: number = 1, search: string = '') : Promise<Page<Comment>> {
+  public getAll(postID: string, page: number = 1, search: string = ''): Promise<Page<Comment>> {
     const url = `${this.baseUrl}/api/comments?postid=${postID}&page=${page}&search=${search}`;
     return this.client.get<Page<Comment>>(url).pipe(catchError(this.handleError<Page<Comment>>('getAll'))).toPromise();
   }
 
   public async post(comment: Comment): Promise<unknown> {
     const url = `${this.baseUrl}/api/comments`;
-    let response = await this.client.post<Comment>(url, comment).pipe(catchError(this.handleError<Comment>('post'))).toPromise();
-    this.update.emit();
-    return response;
+    return this.client.post<Comment>(url, comment).pipe(tap(() => {
+      this.update.emit();
+    }), catchError(this.handleError<Comment>('post'))).toPromise();
   }
 
   public async put(comment: Comment): Promise<unknown> {
     const url = `${this.baseUrl}/api/comments/${comment.id}`;
-    let response = await this.client.put<Comment>(url, comment).pipe(catchError(this.handleError<Comment>('put'))).toPromise();
-    this.update.emit();
-    return response;
+    return this.client.put<Comment>(url, comment).pipe(tap(() => {
+      this.update.emit();
+    }), catchError(this.handleError<Comment>('put'))).toPromise();
   }
 
-  public getReplies(commId: number, postId: string, pageIndex = 1) : Promise<Page<Comment>> {
+  public getReplies(commId: number, postId: string, pageIndex = 1): Promise<Page<Comment>> {
     const url = `${this.baseUrl}/api/comments/${commId}?postID=${postId}&page=${pageIndex}`;
     return this.client.get<Page<Comment>>(url).pipe(catchError(this.handleError<Page<Comment>>('getReplies'))).toPromise();
   }
 
-  public async delete(commId: number) : Promise<unknown> {
+  public async delete(commId: number): Promise<unknown> {
     const url = `${this.baseUrl}/api/comments/${commId}`;
-    let response = await this.client.delete(url).pipe(catchError(this.handleError<Page<Comment>>('getReplies'))).toPromise();
-    this.update.emit();
-    return response;
+    return await this.client.delete(url).pipe(tap(() => {
+      this.update.emit();
+    }), catchError(this.handleError<Page<Comment>>('getReplies'))).toPromise();
   }
 
   private handleError<T>(operation = 'operation', result?: T) {
